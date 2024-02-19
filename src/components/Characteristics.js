2f9a456d47b1e051e57ad73e03664d86682522da
or konstantin <104775108+Orkonstantin@users.noreply.github.com>
2024-02-19 12:32:04 +0200

    Fixed typo

--- src/components/Characteristics.js
+++ src/components/Characteristics.js
@@ -1,54 +1,57 @@
 import React from "react";
-import { Container} from "react-bootstrap";
-import { FaRegLightbulb,FaHandshake,FaLeanpub } from 'react-icons/fa';
+import { Container } from "react-bootstrap";
+import { FaRegLightbulb, FaHandshake, FaLeanpub } from "react-icons/fa";
 const Characteristics = () => {
-
   return (
-      <section id="Characteristics">
-        <Container className="my-5">
-          <h3 className="sectionHeader">Characteristics</h3>
-          <h4 className="text-muted">
-            Let me introduce you some of my strengths
-          </h4>
-          <br/>
-          <div className="row text-center">
-
-            <div className="col-md-4">
+    <section id="Characteristics">
+      <Container className="my-5">
+        <h3 className="sectionHeader">Characteristics</h3>
+        <h4 className="text-muted">
+          Let me introduce you some of my strengths
+        </h4>
+        <br />
+        <div className="row text-center">
+          <div className="col-md-4">
             <span className="fa-stack fa-4x spanIcon">
-              <div className="circleIndex"><FaLeanpub className="iconIndex"/></div>
+              <div className="circleIndex">
+                <FaLeanpub className="iconIndex" />
+              </div>
             </span>
-              <h4 className="service-heading">Autodidact</h4>
-              <p className="text-muted">
-                Has high self-learning ability, with a constant hunger for
-                expanding self-knowledge.
-              </p>
-            </div>
-
-            <div className="col-md-4">
-            <span className="fa-stack fa-4x spanIcon">
-              <div className="circleIndex"><FaHandshake className="iconIndex"/></div>
-            </span>
-              <h4 className="service-heading">Team Player</h4>
-              <p className="text-muted">
-                Has excellent interpersonal skills. Loves working in a team
-                knowing that people are violating each other.
-              </p>
-            </div>
-
-            <div className="col-md-4">
-            <span className="fa-stack fa-4x spanIcon">
-              <div className="circleIndex"><FaRegLightbulb className="iconIndex"/></div>
-            </span>
-              <h4 className="service-heading">Creativity</h4>
-              <p className="text-muted">
-                Creative, with the ability to find solutions quickly and
-                efficiently to problems that arise in real time.
-              </p>
-            </div>
-
+            <h4 className="service-heading">Autodidact</h4>
+            <p className="text-muted">
+              Has high self-learning ability, with a constant hunger for
+              expanding self-knowledge.
+            </p>
           </div>
-        </Container>
-      </section>
+
+          <div className="col-md-4">
+            <span className="fa-stack fa-4x spanIcon">
+              <div className="circleIndex">
+                <FaHandshake className="iconIndex" />
+              </div>
+            </span>
+            <h4 className="service-heading">Team Player</h4>
+            <p className="text-muted">
+              Has excellent interpersonal skills. Loves working in a team,
+              valuing the collaboration with others.
+            </p>
+          </div>
+
+          <div className="col-md-4">
+            <span className="fa-stack fa-4x spanIcon">
+              <div className="circleIndex">
+                <FaRegLightbulb className="iconIndex" />
+              </div>
+            </span>
+            <h4 className="service-heading">Creativity</h4>
+            <p className="text-muted">
+              Creative, with the ability to find solutions quickly and
+              efficiently to problems that arise in real time.
+            </p>
+          </div>
+        </div>
+      </Container>
+    </section>
   );
 };
 export default Characteristics;
